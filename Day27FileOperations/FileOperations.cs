432879e9d769a6bb6b614a4a6e79ec3da721fcac
ManojMichaelX0 <manojthiparapu@gmail.com>
2022-03-27 17:04:33 +0530

    added Day 27 File IO Problems

---
+++ Day27FileOperations/FileOperations.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Day27FileOperations
+{
+    public class FileOperations
+    {
+        public void FileExits()
+        {
+            string path = @"D:\.net\Day27FileOperations\Day27FileOperations\FileOperationDemo.txt";
+            if (File.Exists(path))
+            {
+                Console.WriteLine("File Exists");
+            }
+            else
+            {
+                Console.WriteLine("File Not Found");
+            }
+            Console.ReadKey();
+
+        }
+        public void ReadAllLine()
+        {
+            string path = @"D:\.net\Day27FileOperations\Day27FileOperations\FileOperationDemo.txt";
+            string[] lines;
+            lines = File.ReadAllLines(path);
+            foreach (var l in lines)
+            {
+                Console.WriteLine(l);
+            }
+            Console.ReadKey();
+        }
+        public void ReadAllText()
+        {
+            string path = @"D:\.net\Day27FileOperations\Day27FileOperations\FileOperationDemo.txt";
+
+            string lines = File.ReadAllText(path);
+            Console.WriteLine(lines);
+
+            Console.ReadKey();
+        }
+        public void FileCopy()
+        {
+            string path = @"D:\.net\Day27FileOperations\Day27FileOperations\FileOperationDemo.txt";
+            string copypath = @"D:\.net\Day27FileOperations\Day27FileOperations\FileCopy.txt";
+            File.Copy(path, copypath);
+            Console.ReadKey();
+
+        }
+        public void FileDelete()
+        {
+            string path = @"D:\.net\Day27FileOperations\Day27FileOperations\FileCopy.txt";
+            File.Delete(path);
+
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ Day27FileOperations/StreamReaderAndWriter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Day27FileOperations
+{
+    internal class StreamReaderAndWriter
+    {
+        public void WritingUsingStreamWriter()
+        {
+            string path = @"D:\.net\Day27FileOperations\Day27FileOperations\StreamWriterdemo.txt";
+
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine("Hello World - .net is Awesome ");
+                sw.Close();
+
+                Console.WriteLine(File.ReadAllText(path));
+            }
+            Console.ReadKey();
+        }
+        public void ReadlingUsingStreamReader()
+        {
+            string path = @"D:\.net\Day27FileOperations\Day27FileOperations\StreamWriterdemo.txt";
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string s = " ";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
